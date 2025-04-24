1caf05a428850db097b6381c4358dd7bd29068b3
Bigya <bigyabajracharya@gmail.com>
2025-04-24 13:52:30 -0400

    feat: add GET route

--- src/app/api/tree/route.ts
+++ src/app/api/tree/route.ts
@@ -1,7 +1,7 @@
+import { initDb } from "@/app/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    return NextResponse.json({
-        message: 'Hello World!'
-    })
+    const db = await initDb()
+    return NextResponse.json(db.data.trees)
 }
