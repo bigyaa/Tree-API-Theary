351eb1e822307f5f0845a7aa0178b3e24391256e
Bigya <bigyabajracharya@gmail.com>
2025-04-24 13:40:01 -0400

    feat: setup db

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,3 +1,28 @@
-const db = {}
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+
+export type Treenode = {
+    id: number
+    label: string
+    children: Treenode[]
+}
+
+export type Data = {
+    trees: Treenode[]
+    lastId: number
+}
+
+const adapter = new JSONFile<Data>('db.json')
+const db = new Low<Data>(adapter, {
+    trees: [],
+    lastId: 0
+})
+
+export async function initDb() {
+    await db.read()
+    db.data ||= { trees: [], lastId: 0 }
+    await db.write()
+    return db
+}
 
 export default db;
